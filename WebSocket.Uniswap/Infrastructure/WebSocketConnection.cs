00f4ccd97f4162f633f7fb268d97f1db5efe9635
Roman Yavnikov <roman.yavnikov@redduck.io>
2021-03-28 08:36:21 +0300

    feat: Implemented WebSocket and all configuration for it.

---
+++ WebSocket.Uniswap/Infrastructure/WebSocketConnection.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net.WebSockets;
+using System.Threading;
+using System.Text;
+
+namespace WebSocket.Uniswap.Infrastructure
+{
+    internal class WebSocketConnection
+    {
+        #region Fields
+        private System.Net.WebSockets.WebSocket _webSocket;
+        private int _receivePayloadBufferSize;
+        #endregion
+
+        #region Properties
+        public Guid Id { get; } = Guid.NewGuid();
+
+        public WebSocketCloseStatus? CloseStatus { get; private set; } = null;
+
+        public string CloseStatusDescription { get; private set; } = null;
+        #endregion
+
+        #region Events
+        public event EventHandler<string> ReceiveText;
+
+        public event EventHandler<byte[]> ReceiveBinary;
+        #endregion
+
+        #region Constructor
+        public WebSocketConnection(System.Net.WebSockets.WebSocket webSocket, int receivePayloadBufferSize)
+        {
+            _webSocket = webSocket ?? throw new ArgumentNullException(nameof(webSocket));
+            _receivePayloadBufferSize = receivePayloadBufferSize;
+        }
+        #endregion
+
+        #region Methods
+        public Task SendAsync(string message, CancellationToken cancellationToken)
+        {
+            return _webSocket.SendAsync(buffer: new ArraySegment<byte>(array: Encoding.ASCII.GetBytes(message),
+                                                                  offset: 0,
+                                                                  count: message.Length),
+                                                                  messageType: WebSocketMessageType.Text,
+                                                                  endOfMessage: true,
+                                                                  cancellationToken: cancellationToken);
+        }
+
+        public Task SendAsync(byte[] message, CancellationToken cancellationToken)
+        {
+            return _webSocket.SendAsync(buffer: message,
+                                        messageType: WebSocketMessageType.Text,
+                                        endOfMessage: true,
+                                        cancellationToken: cancellationToken);
+        }
+
+        public async Task ReceiveMessagesUntilCloseAsync()
+        {
+            try
+            {
+                byte[] receivePayloadBuffer = new byte[_receivePayloadBufferSize];
+                WebSocketReceiveResult webSocketReceiveResult = await _webSocket.ReceiveAsync(new ArraySegment<byte>(receivePayloadBuffer), CancellationToken.None);
+                while (webSocketReceiveResult.MessageType != WebSocketMessageType.Close)
+                {
+                    if (webSocketReceiveResult.MessageType == WebSocketMessageType.Binary)
+                    {
+                        var webSocketMessage = Encoding.UTF8.GetString(receivePayloadBuffer).TrimEnd('\0').ToUpper();
+                        if (webSocketMessage == "PING")
+                        {
+                            OnReceivePingPong(webSocketMessage);
+                        }
+                        else
+                           OnReceiveBinary(receivePayloadBuffer);
+                    }
+                    else
+                    {
+                        var webSocketMessage = Encoding.UTF8.GetString(receivePayloadBuffer).TrimEnd('\0').ToUpper();
+                        if (webSocketMessage == "PING")
+                        {
+                            OnReceivePingPong(webSocketMessage);
+                        }
+                        else
+                            OnReceiveText(webSocketMessage);
+                    }
+
+                    webSocketReceiveResult = await _webSocket.ReceiveAsync(new ArraySegment<byte>(receivePayloadBuffer), CancellationToken.None);
+                }
+
+                CloseStatus = webSocketReceiveResult.CloseStatus.Value;
+                CloseStatusDescription = webSocketReceiveResult.CloseStatusDescription;
+            }
+            catch (WebSocketException wsex) when (wsex.WebSocketErrorCode == WebSocketError.ConnectionClosedPrematurely)
+            { }
+        }
+
+        private void OnReceiveText(string webSocketMessage)
+        {
+            ReceiveText?.Invoke(this, webSocketMessage);
+        }
+
+        private void OnReceiveBinary(byte[] webSocketMessage)
+        {
+            ReceiveBinary?.Invoke(this, webSocketMessage);
+        }
+
+        private void OnReceivePingPong(string webSocketMessage)
+        {
+            webSocketMessage = "PONG";
+            ReceiveText?.Invoke(this, webSocketMessage);
+        }
+
+        #endregion
+    }
+}
