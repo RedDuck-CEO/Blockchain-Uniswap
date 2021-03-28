cd5a5f2be757c39350ab1e824e12f6b6d8f51e13
Roman Yavnikov <roman.yavnikov@redduck.io>
2021-03-28 08:38:01 +0300

    feat: Created u-test for WebSocket.

---
+++ Test.Uniswap/WebSocket/WebSocketTest.cs
@@ -0,0 +1,117 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Diagnostics;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Test.Uniswap
+{
+    [TestClass]
+    public class WebSocketTest
+    {
+        private const string webSocketURL = "wss://localhost:5001/socket";
+
+        [TestMethod]
+        public async Task PlayPingPongWebSocketTest()
+        {
+            var buffer = new byte[1024 * 4];
+
+            const string message = "PING";
+            const string responseFromWebSocket = "PONG";
+
+            string response = string.Empty;
+
+            var client = await CreateConnectWithLocalWebSocket();
+
+            for (int i = 0; i < 5; i++)
+            {
+                var bytes = Encoding.UTF8.GetBytes(message);
+                await client.SendAsync(new ArraySegment<byte>(bytes), WebSocketMessageType.Text, true, CancellationToken.None);
+
+                var result = await client.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+
+                response = Encoding.UTF8.GetString(buffer, 0, result.Count).TrimEnd('\0').ToUpper();
+            }
+
+            await client.CloseOutputAsync(WebSocketCloseStatus.NormalClosure, "", CancellationToken.None);
+
+            Assert.AreEqual(responseFromWebSocket, response);
+        }
+
+        [TestMethod]
+        public async Task GetUniswapHeartbeatTest()
+        {
+            bool isCompleted = false;
+            var buffer = new byte[1024 * 4];
+            int actualCounterUniswapHeartbeat = 0;
+            int expectedCounterUniswapHeartbeat = 10;
+
+            try
+            {
+                var client = await CreateConnectWithLocalWebSocket();
+
+                while (!isCompleted)
+                {
+                    var result = await client.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+
+                    if (result.MessageType == WebSocketMessageType.Text)
+                    {
+                        var webSocketMessage = Encoding.UTF8.GetString(buffer, 0, result.Count).TrimEnd('\0');
+                        actualCounterUniswapHeartbeat = webSocketMessage == "Uniswap Heartbeat" ? ++actualCounterUniswapHeartbeat : actualCounterUniswapHeartbeat;
+
+                        Assert.AreEqual("Uniswap Heartbeat", webSocketMessage);
+
+                        if (actualCounterUniswapHeartbeat >= 10)
+                            isCompleted = true;
+                    }
+                    else if (result.MessageType == WebSocketMessageType.Close)
+                    {
+                        await client.CloseOutputAsync(WebSocketCloseStatus.NormalClosure, "", CancellationToken.None);
+                        break;
+                    }
+                }
+
+                Assert.AreEqual(expectedCounterUniswapHeartbeat, actualCounterUniswapHeartbeat);
+                Assert.IsTrue(isCompleted);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        #region Helpers
+
+        private static async Task<string> Receiving(ClientWebSocket client)
+        {
+            var buffer = new byte[1024 * 4];
+
+            while (true)
+            {
+                var result = await client.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+
+                if (result.MessageType == WebSocketMessageType.Text)
+                {
+                    return Encoding.UTF8.GetString(buffer, 0, result.Count).TrimEnd('\0');
+                }
+                else if (result.MessageType == WebSocketMessageType.Close)
+                {
+                    await client.CloseOutputAsync(WebSocketCloseStatus.NormalClosure, "", CancellationToken.None);
+                    break;
+                }
+            }
+            return null;
+        }
+
+        private async Task<ClientWebSocket> CreateConnectWithLocalWebSocket()
+        {
+            var client = new ClientWebSocket();
+            await client.ConnectAsync(new Uri(webSocketURL), CancellationToken.None);
+            return client;
+        }
+
+        #endregion
+    }
+}
